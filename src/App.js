d0871b8803ce6eb49decc2f46b7e4dd495b73ae4
naif-alghamdi <naif.gh66@gmail.com>
2024-01-19 01:27:20 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [task, setTask] = useState([])
+  const getInput = useRef()
+
+  const handelTaskInput = () => {
+    const newTask = getInput.current.value
+    const addNewTask = {complated: false, task: newTask}
+    setTask([...task, addNewTask])
+  }
+
+  const handelItemClick = (index) => {
+    const updateTask = [...task]
+    updateTask[index].complated = !updateTask[index].complated
+    setTask(updateTask)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do list</h1>
+      <div className='todo-contaner'>
+        <ul>
+          {task.map(({task, complated}, index) => {return <li className={complated ? "done" : "" } onClick={() => handelItemClick(index)} key={index}>{task}</li>})}
+        </ul>
+        <input ref={getInput}></input>
+        <button onClick={handelTaskInput}>Add Task</button>
+      </div>
     </div>
   );
 }
